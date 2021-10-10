57c482a9e08620ce60dbdaf6979d387f0f8f5b42
Danny-Dasilva <yahchayildasilva@gmail.com>
2021-10-10 16:22:21 -0400

    another change

--- my-package/index.js
+++ my-package/index.js
@@ -1,5 +1,5 @@
 console.log("hello from my-package.")
 
-const sayHello = (name) => console.log(`TESTING, how is it going ${name}`)
+const sayHello = (name) => console.log(`TESTING Example2, how is it going ${name}`)
 
 module.exports = {sayHello}
